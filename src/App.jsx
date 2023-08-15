5e1508108d8f4a2c0a557b9d40756d6091c42f73
Adithyan k P <adithyankp777@gmail.com>
2023-08-15 09:56:48 +0530

    initial vite project creation

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Happy hacking</p>
+      </div>
+    </>
+  )
+}
+
+export default App
